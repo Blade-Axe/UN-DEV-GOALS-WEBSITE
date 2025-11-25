b1af0997cc650756fa62f526ce21aff9d269a9ee
mac9078 <macauley.bacon230607@gmail.com>
2025-11-25 12:23:37 +0000

    set env up

--- app/app.js
+++ app/app.js
@@ -23,6 +23,10 @@ app.get('/the-team', (req, res) => {
     res.render('the-team');
 });
 
+app.get('/goals', (req, res) => {
+    res.render('goals');
+});
+
 /* 
 Advanced Approach: Render EJS template
 with asynchronously fetched data
