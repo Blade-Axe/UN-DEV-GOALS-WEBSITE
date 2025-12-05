50e31255b618f10ae18e6743793c5c6d144d339b
BladeAxe <Madaxam@protonmail.com>
2025-12-05 15:47:42 +0000

    initial start on newsletter

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes');
+const { body, validationResult } = require("express-validator");
+const fs = require('fs');
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -9,6 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Set up static file serving
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     res.locals.routes = routes;
@@ -27,6 +31,43 @@ app.get(routes.team, (req, res) => {
 
 app.get(routes.subscribe, (req, res) => {
     res.render('subscribe', { title: 'SUBSCRIBE' });
+});
+
+app.post(routes.subscribe, (req, res) => {
+    console.log(req.body);
+    const newSubscriber = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userEmail: req.body.userEmail,
+        comments: req.body.userSubmission 
+    };
+
+    const filePath = path.join(__dirname, 'data.json');
+
+    //Read existing file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        let subscribers = [];
+        if (!err && data) {
+            try {
+                subscribers = JSON.parse(data);
+            } catch (e) {
+                console.error("Error parsing JSON", e);
+            }
+        }
+
+        //Add new subscriber
+        subscribers.push(newSubscriber);
+
+        //Write back to file
+        fs.writeFile(filePath, JSON.stringify(subscribers, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to save data' });
+            }
+            //Send success response back to frontend
+            res.json(newSubscriber); 
+        });
+    });
 });
 
 app.get(routes.goals, (req, res) => {
